c8ea55c2028f771f6b1b4a3dbe7a7f52e8289219
Wendy <xu.wenhit@gmail.com>
2017-04-27 22:00:36 -0400

    found new explaination of question 2

--- codebook.md
+++ codebook.md
@@ -3,20 +3,21 @@
 
 | Files Names (.txt) | Data Sets in R |  Dimension  |
 |     :---:      | :---:      | :---:      |
-| features      | featureNames | 561*2 |
-| activity_labels      | activityNames      |   6*1 |
-| X_train |      trainData |  7352*561   |
-| X_test |  testData | 2947*561|
-| subject_train| trainPeople |  7352*1|
-|subject_test| testPeople| 2947*1 |
-|y_train| trainActivity| 7352*1 |
-|y_test| testActivity| 2947*1 |
+| features      | feature | 561*2 |
+| activity_labels      | activity      |   6*1 |
+| X_train |      train |  7352*561   |
+| X_test |  test | 2947*561|
+| subject_train| train_subject |  7352*1|
+|subject_test| test_subject| 2947*1 |
+|y_train| train_activity| 7352*1 |
+|y_test| test_activity| 2947*1 |
 
 
 # Below is the table for the new data sets created after cleaning and analysing the raw data.
 | Data Sets in R |  Dimension  | Description
 |     :---:      | :---:      | :---:      |
 | DATA      |   10299*563 | Merge from the last 6 data sets from above table (Qestion 1) |
-| Measure |    561*2 |  Extracts the mean and standard deviation for each measurement (Qestion 2)   |
-| DATA2      | 180*563      |  Group by Activity and People, then summarize all measurements by mean (Question 5)  |
+| DATA_left |   10299*81| Extracts only the measurements on the mean and standard deviation for each measurement (Q2) |
+| clean_data      | 180*81      |  Group by Activity and Subject, then summarize all features by mean (Question 5)  |
 
+## Notice that if you read "clean_data.txt" into R, then dimension would be 181*81 rather than 180*81 since it automatically add V1 to V81 to this.
--- run_analysis.R
+++ run_analysis.R
@@ -4,32 +4,45 @@ library(dplyr)
 
 
 
-# Now suppose all text files are in the working directory 
-# read them into R and give them new names
-featureNames <- read.table("features.txt")
-activityNames <- read.table("activity_labels.txt")
-trainData <- read.table("X_train.txt")
-testData <- read.table("X_test.txt")
-trainPeople <- read.table("subject_train.txt")
-library(plyr)
-library(dplyr)
-testPeople <- read.table("subject_test.txt")
-trainActivity<-read.table("y_train.txt")
-testActivity<-read.table("y_test.txt")
+# Get the raw data from the website if no data in the working directory.
+filename <- "getdata_dataset.zip"
+
+if (!file.exists(filename)){
+  fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip "
+  download.file(fileURL, filename)
+}  
+if (!file.exists("UCI HAR Dataset")) { 
+    unzip(filename) 
+}
+
+
+# Read data into R and rename them the way easy to understand
+feature <- read.table("UCI HAR Dataset/features.txt")
+activity <- read.table("UCI HAR Dataset/activity_labels.txt")
+train <- read.table("UCI HAR Dataset/train/X_train.txt")
+test <- read.table("UCI HAR Dataset/test/X_test.txt")
+train_subject <- read.table("UCI HAR Dataset/train/subject_train.txt")
+test_subject <- read.table("UCI HAR Dataset/test/subject_test.txt")
+train_activity <- read.table("UCI HAR Dataset/train/y_train.txt")
+test_activity <- read.table("UCI HAR Dataset/test/y_test.txt")
+
 
 
 
 # Rename the column names 
-colnames(trainPeople) <- "People"
-colnames(testPeople) <- "People"
-colnames(trainActivity) <- "Activity"
-colnames(testActivity) <- "Activity"
+colnames(train_subject) <- "Subject"
+colnames(test_subject) <- "Subject"
+colnames(train_activity) <- "Activity"
+colnames(test_activity) <- "Activity"
+
+
 
 
 
 # Now merge the data sets with other infomation data
-Train <- cbind(trainPeople,trainActivity,trainData)
-Test <- cbind(testPeople,testActivity,testData)
+Train <- cbind(train_subject,train_activity,train)
+Test <- cbind(test_subject,test_activity,test)
+
 
 
 # Now merge training and testing to get a whole data as Q1 asks
@@ -37,37 +50,47 @@ DATA <- rbind(Train,Test)
 
 
 
-# Extract the measurements on the mean and standard deviation for each measurements as Q2 asks
-Measure <- data.frame(meanV=sapply(DATA[,3:563],mean),sdV=sapply(DATA[,3:563],sd))
+# Extracts only the measurements on the mean and standard deviation for each measurement (Q2)
+# First get the index number of the required measurements in feature file
+# Then notice that all the index number of features in DATA are 2 more than their index in feature
+# Combine index of measurement required and index for subject and activity (1 and 2)
+# Select those columns, that's the data we want 
+features_left <- grep(".*mean.*|.*std.*", feature[,2])
+index_features_left <- features_left + 2
+index_left <- c(1,2,index_features_left)
+DATA_left <- select(DATA,index_left)
 
 
-# Use descriptive activity names to name the activities in DATA as Q3 asks
-# Compare the number in DATA$Activity column to the number in the first column of activityNames file
-# Then pass the descriptive activity name in the second column of activityNames file to DATA
-for (i in 1:nrow(DATA)) 
+
+# Use descriptive activity names to name the activities in DATA_left as Q3 asks
+# Compare the number in DATA_left$Activity column to the number in the first column of activity file
+# Then pass the descriptive activity name in the second column of activity file to DATA_left
+for (i in 1:nrow(DATA_left)) 
 {
    for (j in 1:6) {
-        if (DATA$Activity[i]==activityNames[[1]][j])
-        { DATA$Activity[i]<-as.character(activityNames[[2]][j])}
+        if (DATA_left$Activity[i]==activity[[1]][j])
+        { DATA_left$Activity[i]<-as.character(activity[[2]][j])}
 }
 }
+
 
 
 # Labels the data set with descriptive variable names as Q4 asks
-# Use names in featureNames file to rename the column (from 3 to 562) names in DATA
-colnames(DATA)[3:563]<-as.character(featureNames[[2]])
+# Use names in feature file to rename the measurement columns in DATA_left
+# Recall that features_left are indices of required measurement in feature file
+names(DATA_left)[3:ncol(DATA_left)]<-as.character(feature[[2]][features_left])
+
 
 
 # Q5: creates a second, independent tidy data set with the average of each variable for each activity and each person.
 # Notice: group by two variables (in class we only see group by one variable)
-# Notice: summarize more than one (561) variables (in class we only see summarize one variable)
-# So I use summarise_each function to handle summary more than one variable, but it gives me error if I use colnames in DATA
-# But if I rename the columns by V1 to V561, it works. That's why I rename them
-# After collecting the means we want (stored in DATA2 file), I give new descriptive column names to DATA2. (using "paste") 
-colName <-colnames(DATA)[3:563]
-colNameData2 <- sapply("Mean",paste,colName,sep="_")
-colnames(DATA)[3:563] <- colnames(Test)[3:563]
-byPeopleActivity <- group_by( DATA, People, Activity)
-DATA2 <- summarise_each(byPeopleActivity,funs(mean))
-colnames(DATA2)[3:563] <- colNameData2
-write.table(DATA2,"DATA2.txt",row.names=F)
+# Notice: summarize more than one (79) variables (in class we only see summarize one variable)
+# So I use summarise_each function to handle summary more than one variable
+# I give new descriptive column names to clean_data. (using "paste") 
+# At last write clean_data to a text file named "clean_data"
+colName <- colnames(DATA_left)[3:ncol(DATA_left)]
+name_clean <- sapply("Mean",paste,colName,sep="_")
+by_subject_activity <- group_by( DATA_left, Subject, Activity)
+clean_data <- summarise_each(by_subject_activity,funs(mean))
+colnames(clean_data)[3:ncol(DATA_left)] <- name_clean
+write.table(clean_data,"clean_data.txt",row.names=F)
